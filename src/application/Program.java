19c5b0487648cf367dce94efbc8f24a554aa2674
Carlos Santos <carloscoelho1226@gmail.com>
2022-12-14 14:00:04 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -62,6 +62,15 @@ public class Program {
 			System.out.println("Inserted! New id = " + newSeller.getId());
 			break;
 		}
+		case 5: {
+			System.out.println("\n=== TEST 5: seller update ===");
+			Seller seller = null;
+			seller = sellerDao.findById(1);
+			seller.setName("Martha Waine");
+			sellerDao.update(seller);
+			System.out.println("Update completed");
+			break;
+		}	
 		default:
 			throw new IllegalArgumentException("Unexpected value: " + opcao);
 		}		
