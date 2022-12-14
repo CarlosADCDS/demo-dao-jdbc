4ae3e1ff8096fd3b3c14efb40619bd23d1330b4e
Carlos Santos <carloscoelho1226@gmail.com>
2022-12-14 13:12:09 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.text.ParseException;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,11 +12,31 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Para buscar por id, digite 1.\n"
+				+ "Para buscar por departamento, digite 2.\n"
+				+ "Para buscar todos, digite 3.\n"
+				+ "Para inserir, digite 4.\n"
+				+ "Para realizar um update, digite 5.\n"
+				+ "Para deletar por id, digite 6.");
+		int opcao = sc.nextInt();
 		
-		Seller seller = sellerDao.findById(3);
+		if(opcao != 6) {
+			sc.close();
+		}
 		
-		System.out.println(seller);
-		
+		switch (opcao) {
+		case 1: {
+			System.out.println("=== TEST 1: seller findByID ===");
+			Seller seller = sellerDao.findById(3);
+			System.out.println(seller);
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + opcao);
+		}
 	}
 
 }
